46be0d5df6c8623b4bae2fa84215e07c4057b4c1
Judith Rodriguez <rodriguezsalinasperla@gmail.com>
2024-02-22 12:16:12 -0600

    Tamanio del forms mas big

--- Biblioteca/Form1.Designer.cs
+++ Biblioteca/Form1.Designer.cs
@@ -158,7 +158,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(880, 450);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(comboBox2);
