68a571c15f6691ef442e20d5e137216e6949cd82
Jadir Miramontes <jadirescuela23@gmail.com>
2024-03-04 12:23:11 -0600

    trabajo finiquitado

--- Biblioteca/Form1.Designer.cs
+++ Biblioteca/Form1.Designer.cs
@@ -29,18 +29,15 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            label2 = new Label();
             label3 = new Label();
             label4 = new Label();
             label5 = new Label();
-            label6 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            comboBox1 = new ComboBox();
-            comboBox2 = new ComboBox();
-            button1 = new Button();
-            button2 = new Button();
+            tbPrecio = new TextBox();
+            cbDescuento = new ComboBox();
+            cbArea = new ComboBox();
+            btnCalcular = new Button();
+            btnTerminar = new Button();
+            rtbRegistros = new RichTextBox();
             SuspendLayout();
             // 
             // label1
@@ -53,19 +50,10 @@
             label1.TabIndex = 0;
             label1.Text = "MUSEO DEL DESIERTO";
             // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(52, 103);
-            label2.Name = "label2";
-            label2.Size = new Size(26, 15);
-            label2.TabIndex = 1;
-            label2.Text = "No.";
-            // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(51, 149);
+            label3.Location = new Point(63, 84);
             label3.Name = "label3";
             label3.Size = new Size(72, 15);
             label3.TabIndex = 2;
@@ -74,7 +62,7 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(52, 194);
+            label4.Location = new Point(331, 84);
             label4.Name = "label4";
             label4.Size = new Size(36, 15);
             label4.TabIndex = 3;
@@ -83,94 +71,77 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(49, 248);
+            label5.Location = new Point(593, 84);
             label5.Name = "label5";
             label5.Size = new Size(47, 15);
             label5.TabIndex = 4;
             label5.Text = "PRECIO";
             // 
-            // label6
+            // tbPrecio
             // 
-            label6.AutoSize = true;
-            label6.Location = new Point(566, 103);
-            label6.Name = "label6";
-            label6.Size = new Size(44, 15);
-            label6.TabIndex = 5;
-            label6.Text = "FECHA";
+            tbPrecio.Location = new Point(673, 81);
+            tbPrecio.Name = "tbPrecio";
+            tbPrecio.Size = new Size(148, 23);
+            tbPrecio.TabIndex = 7;
             // 
-            // textBox1
+            // cbDescuento
             // 
-            textBox1.Location = new Point(84, 100);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(85, 23);
-            textBox1.TabIndex = 6;
+            cbDescuento.FormattingEnabled = true;
+            cbDescuento.Location = new Point(141, 81);
+            cbDescuento.Name = "cbDescuento";
+            cbDescuento.Size = new Size(136, 23);
+            cbDescuento.TabIndex = 9;
             // 
-            // textBox2
+            // cbArea
             // 
-            textBox2.Location = new Point(129, 245);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(148, 23);
-            textBox2.TabIndex = 7;
+            cbArea.FormattingEnabled = true;
+            cbArea.Location = new Point(408, 81);
+            cbArea.Name = "cbArea";
+            cbArea.Size = new Size(136, 23);
+            cbArea.TabIndex = 10;
             // 
-            // textBox3
+            // btnCalcular
             // 
-            textBox3.Location = new Point(631, 95);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(148, 23);
-            textBox3.TabIndex = 8;
+            btnCalcular.Location = new Point(305, 126);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(81, 30);
+            btnCalcular.TabIndex = 11;
+            btnCalcular.Text = "CALCULAR";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
             // 
-            // comboBox1
+            // btnTerminar
             // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(129, 146);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(136, 23);
-            comboBox1.TabIndex = 9;
-            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            btnTerminar.Location = new Point(508, 128);
+            btnTerminar.Name = "btnTerminar";
+            btnTerminar.Size = new Size(101, 28);
+            btnTerminar.TabIndex = 12;
+            btnTerminar.Text = "TERMINAR";
+            btnTerminar.UseVisualStyleBackColor = true;
+            btnTerminar.Click += btnTerminar_Click;
             // 
-            // comboBox2
+            // rtbRegistros
             // 
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(129, 191);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(136, 23);
-            comboBox2.TabIndex = 10;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(283, 240);
-            button1.Name = "button1";
-            button1.Size = new Size(81, 30);
-            button1.TabIndex = 11;
-            button1.Text = "CALCULAR";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(360, 313);
-            button2.Name = "button2";
-            button2.Size = new Size(101, 28);
-            button2.TabIndex = 12;
-            button2.Text = "TERMINAR";
-            button2.UseVisualStyleBackColor = true;
+            rtbRegistros.Location = new Point(272, 162);
+            rtbRegistros.Name = "rtbRegistros";
+            rtbRegistros.Size = new Size(337, 215);
+            rtbRegistros.TabIndex = 13;
+            rtbRegistros.Text = "";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(880, 450);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(comboBox2);
-            Controls.Add(comboBox1);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
-            Controls.Add(label6);
+            Controls.Add(rtbRegistros);
+            Controls.Add(btnTerminar);
+            Controls.Add(btnCalcular);
+            Controls.Add(cbArea);
+            Controls.Add(cbDescuento);
+            Controls.Add(tbPrecio);
             Controls.Add(label5);
             Controls.Add(label4);
             Controls.Add(label3);
-            Controls.Add(label2);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Sistema de registro";
@@ -181,17 +152,14 @@
         #endregion
 
         private Label label1;
-        private Label label2;
         private Label label3;
         private Label label4;
         private Label label5;
-        private Label label6;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private ComboBox comboBox1;
-        private ComboBox comboBox2;
-        private Button button1;
-        private Button button2;
+        private TextBox tbPrecio;
+        private ComboBox cbDescuento;
+        private ComboBox cbArea;
+        private Button btnCalcular;
+        private Button btnTerminar;
+        private RichTextBox rtbRegistros;
     }
 }
--- Biblioteca/Form1.cs
+++ Biblioteca/Form1.cs
@@ -2,14 +2,107 @@ namespace Biblioteca
 {
     public partial class Form1 : Form
     {
+        private List<AreaMuseo> areasMuseo = new List<AreaMuseo>()
+        {
+            new AreaMuseo("Exposición Principal", 40.0m),
+            new AreaMuseo("Galería de Arte", 55.0m),
+            new AreaMuseo("Zona Infantil", 35.0m)
+        };
+
+        private int numeroEntrada = 1; // contador para el número de entrada
+
         public Form1()
         {
             InitializeComponent();
+            // Configurar ComboBox de Descuento
+            cbDescuento.Items.Add("Adulto (Sin descuento)");
+            cbDescuento.Items.Add("Mayor de Edad (50% descuento)");
+            cbDescuento.Items.Add("Niño (15% descuento)");
+
+            // Configurar ComboBox de Área
+            foreach (var area in areasMuseo)
+            {
+                cbArea.Items.Add(area.Nombre);
+            }
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void btnCalcular_Click(object sender, EventArgs e)
         {
+            decimal precioBase = 40; // precio base predeterminado
+            decimal descuento = 0;
 
+            // Obtener precio base del área seleccionada
+            if (cbArea.SelectedItem != null)
+            {
+                string nombreAreaSeleccionada = cbArea.SelectedItem.ToString();
+                precioBase = ObtenerPrecioPorNombreArea(nombreAreaSeleccionada);
+            }
+
+            // Calcular descuento
+            if (cbDescuento.SelectedIndex == 1) // Mayor de Edad
+            {
+                descuento = precioBase * 0.5m;
+            }
+            else if (cbDescuento.SelectedIndex == 2) // Niño
+            {
+                descuento = precioBase * 0.15m;
+            }
+
+            // Calcular precio final
+            decimal precioFinal = precioBase - descuento;
+            tbPrecio.Text = precioFinal.ToString("C");
+        }
+
+        private void btnTerminar_Click(object sender, EventArgs e)
+        {
+            // Obtener la fecha actual
+            string fechaActual = DateTime.Now.ToString("dd/MM/yyyy");
+
+            // Guardar campos en el RichTextBox
+            string registro = $"Número de Entrada: {numeroEntrada}\n" +
+                              $"Descuento: {cbDescuento.SelectedItem}\n" +
+                              $"Área Visitada: {cbArea.SelectedItem}\n" +
+                              $"Precio: {tbPrecio.Text}\n" +
+                              $"Fecha: {fechaActual}\n\n";
+
+            rtbRegistros.AppendText(registro);
+
+            // Incrementar el número de entrada
+            numeroEntrada++;
+
+            // Limpiar campos después de guardar
+            LimpiarCampos();
+        }
+
+        private void LimpiarCampos()
+        {
+            cbDescuento.SelectedIndex = -1;
+            cbArea.SelectedIndex = -1;
+            tbPrecio.Clear();
+        }
+
+        private decimal ObtenerPrecioPorNombreArea(string nombreArea)
+        {
+            foreach (var area in areasMuseo)
+            {
+                if (area.Nombre == nombreArea)
+                {
+                    return area.Precio;
+                }
+            }
+            return 0; // Si no se encuentra el área, retornar un precio de 0
+        }
+    }
+
+    public class AreaMuseo
+    {
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+
+        public AreaMuseo(string nombre, decimal precio)
+        {
+            Nombre = nombre;
+            Precio = precio;
         }
     }
 }
